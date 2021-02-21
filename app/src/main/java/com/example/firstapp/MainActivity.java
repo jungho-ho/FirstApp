9e04b8cf19917da0de4dd8378f7cefca764341e3
jungho-ho <ggtmone1@gmail.com>
2021-02-22 00:17:46 +0900

    우선 첫번째 80% 진도율.

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -21,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = (TextView) findViewById(R.id.txt_hello);
         textView.setText("It's beginnig!");
         check_editText();//공백 확인.
-
+        calcu_Percent();
     }
 
     private void check_editText(){
@@ -39,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
             tvID = getResources().getIdentifier("txt_"+i, "id", getPackageName());
             ttView[i] = (TextView)findViewById(tvID);
 
-            if(edittText[i].length() == 0){
+            if(edittText[i].length() == 0 && etID != 4 && etID != 5 && etID != 7 && etID != 8){
                 builder.setTitle("확인하세요.").setMessage(ttView[i].getText() + "(이)가 공백입니다.");
                 AlertDialog alertDialog = builder.create();
                 alertDialog.show();
@@ -47,5 +46,38 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+    }
+
+    private void calcu_Percent(){
+        calcu_대출이자();
+        calcu_수익률();
+    }
+
+    private void calcu_수익률(){
+        EditText 투자금 = (EditText) findViewById(R.id.editTxt_0);
+        EditText 월세 = (EditText) findViewById(R.id.editTxt_6);
+        EditText 년_대출이자 = (EditText) findViewById(R.id.editTxt_5);
+
+        int 년수익 = Integer.parseInt(월세.getText().toString()) * 12 - Integer.parseInt(년_대출이자.getText().toString());
+
+        EditText d = (EditText) findViewById(R.id.editTxt_8);
+        d.setText(String.format("%d",년수익));//8.수익금/년
+
+        double 수익률 = (년수익 * 100) / (double)Integer.parseInt(투자금.getText().toString());
+        EditText dd = (EditText) findViewById(R.id.editTxt_7);
+        dd.setText(String.format("%.1f", 수익률));//7.수익률
+    }
+
+    private void calcu_대출이자(){
+        EditText 대출금 = (EditText) findViewById(R.id.editTxt_2);
+        EditText 금리 = (EditText) findViewById(R.id.editTxt_3);
+
+        double 년이자 = Integer.parseInt(대출금.getText().toString()) * (double)(Integer.parseInt(금리.getText().toString()) / (double)100);
+
+        EditText dd = (EditText) findViewById(R.id.editTxt_5);
+        dd.setText(String.format("%.0f", 년이자));//5.년이자
+
+        EditText d = (EditText) findViewById(R.id.editTxt_4);
+        d.setText(String.format("%.0f",년이자/12));//4.월이자
     }
 }
