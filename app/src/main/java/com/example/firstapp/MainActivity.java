700f7044230db45b0398b5a026c64ef53fca3085
jungho-ho <ggtmone1@gmail.com>
2021-02-21 18:32:11 +0900

    버튼 컨트롤의 onClick속성에 맘에 드는 method 이름을 입력해 넣고, ~xml에 해당 속성에 마우스를 올리면, method를 만들겠냐?는 tooltip이 나타난다. ㅎㅎ

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick_btn_hello_text_change(View view) {
+        TextView textView = (TextView) findViewById(R.id.txt_hello);
+        textView.setText("It's beginnig!");
     }
 }
