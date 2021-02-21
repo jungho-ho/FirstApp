2980cbc74cc0768f8dfa42683abfc935c29f8dbf
jungho-ho <ggtmone1@gmail.com>
2021-02-21 21:25:27 +0900

    컨트롤(EditText)를 for문으로 변수를 사용하여 가져오고 싶은데, 못함.

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.firstapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,5 +20,35 @@ public class MainActivity extends AppCompatActivity {
     public void onClick_btn_hello_text_change(View view) {
         TextView textView = (TextView) findViewById(R.id.txt_hello);
         textView.setText("It's beginnig!");
+        check_editText();
+    }
+
+    private void check_editText(){
+
+//        ViewGroup viewGroup = (ViewGroup)findViewById(R.id.rootView);
+//        viewGroup.getChildCount();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        EditText edittText;
+
+        for(int i=0; i < 9; i++ ){
+            edittText = (EditText)findViewById(R.id.editTxt_ + i);
+            if(edittText.getText().toString() == ""){
+//                builder.setTitle("확인하세요.").setMessage(viewGroup.getChildCount());
+                builder.setTitle("확인하세요.").setMessage(edittText.getId() + "가 공백입니다.");
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+                break;
+            }
+        }
+
+//        builder.setTitle("확인하세요.").setMessage(viewGroup.getChildCount());
+//
+//        AlertDialog alertDialog = builder.create();
+//
+//        alertDialog.show();
+
+        //for(TextInputEditText editText :
     }
 }
