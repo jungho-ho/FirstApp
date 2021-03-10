29ec775a7b275bb22c685e252510a6b32595ea29
jungho-ho <ggtmone1@gmail.com>
2021-03-10 21:25:29 +0900

    99%완료, 숫자만 입력하는 Control이라 쪼매 그렇지만,

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -70,18 +70,18 @@ public class MainActivity extends AppCompatActivity {
         int 년수익 = Integer.parseInt(월세.getText().toString()) * 12 - Integer.parseInt(년_대출이자.getText().toString());
         Log.v(Tag + "_년수익", String.valueOf(년수익));
         EditText d = (EditText) findViewById(R.id.editTxt_8);
-        d.setText(String.format("%d",년수익));//8.수익금/년
+        d.setText(String.format("%,d",년수익));//8.수익금/년
 
-        double 수익률 = (년수익 * 100) / (double)Integer.parseInt(투자금.getText().toString());
+        double 수익률 = 년수익 / (double)(Integer.parseInt(투자금.getText().toString())) * 100;
         EditText dd = (EditText) findViewById(R.id.editTxt_7);
-        dd.setText(String.format("%.1f", 수익률));//7.수익률
+        dd.setText(String.format("%.0f", 수익률));//7.수익률
     }
 
     private void calcu_대출이자(){
-        EditText 대출금 = (EditText) findViewById(R.id.editTxt_2);
+        EditText 대출원금 = (EditText) findViewById(R.id.editTxt_2);
         EditText 금리 = (EditText) findViewById(R.id.editTxt_3);
 
-        double 년이자 = Integer.parseInt(대출금.getText().toString()) * (double)(Integer.parseInt(금리.getText().toString()) / (double)100);
+        double 년이자 = (Integer.parseInt(대출원금.getText().toString()))*((double)(Integer.parseInt(금리.getText().toString())) / (double)100);
         Log.v(Tag + "_금리", String.valueOf((double)(Integer.parseInt(금리.getText().toString()) / (double)100)));
 
         EditText dd = (EditText) findViewById(R.id.editTxt_5);
